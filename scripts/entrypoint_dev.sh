47af70b76c4e7ee764f5f18e2b76eb05414515bd
Ignacio <ivargas@sicom.cl>
2023-07-28 01:33:39 -0400

    Fix conditional in entrypoint script

--- scripts/entrypoint_dev.sh
+++ scripts/entrypoint_dev.sh
@@ -4,7 +4,7 @@ set -e
 
 DB_PATH="$APP_HOME/db.sqlite3"
 
-if [ -f "$DB_PATH" ]; then
+if [ ! -f "$DB_PATH" ]; then
     echo "Database not found!"
     echo "Making migrations"
     python manage.py makemigrations
