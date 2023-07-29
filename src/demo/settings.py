a77e13d422eded73e9d86cfe2283ccf68b811a7e
ignvb <ignaciovargasbustos@gmail.com>
2023-07-29 02:48:24 -0400

    Update demo/settings.py: Changed DEBUG to False for running in production

--- src/demo/settings.py
+++ src/demo/settings.py
@@ -14,7 +14,7 @@ env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
